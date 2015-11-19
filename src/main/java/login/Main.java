80a0a432223b2ddbe0fca88386d0979a2ede358b
iamjohnnyzhuang <249602015@qq.com>
2015-11-19 12:29:01 +0800

    完成，接下来放到服务器上一小时执行一次即可。

--- src/main/java/login/Main.java
+++ src/main/java/login/Main.java
@@ -2,72 +2,80 @@ package login;
 
 import utils.*;
 
+import javax.mail.MessagingException;
+
 
 /**
  * Created by zhuangjy on 2015/11/18.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("======================首先登陆会收到302重定向======================");
+        System.out.println("======================首先登陆会收到302重定向提取重定向URL======================");
         String url = "http://59.77.226.32/logincheck.asp";
         String body = "muser=031202338&passwd=123123&x=42&y=19";
         HttpRequest request = new HttpRequest();
-        request.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        //设置Header模拟浏览器
+        request.addHeader("Content-Type", "application/x-www-form-urlencoded");
         request.addHeader("Connection", "keep-alive");
+        request.addHeader("Cache-Control","private");
+        request.addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        request.addHeader("Upgrade-Insecure-Requests","1");
+        request.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36");
+        request.addHeader("Referer","http://jwch.fzu.edu.cn/");
+        request.addHeader("Accept-Encoding","gzip, deflate, sdch");
+        request.addHeader("Accept-Language","zh-CN,zh;q=0.8,en;q=0.6");
+        request.addHeader("Cookie", "ASP.NET_SessionId=ajo0vavbnibugcrvlbrwp1ki");
+
         request.setBody(body);
         HttpResponse httpResponse = HttpClientUtil.doRequestWithNoRetry(url, HttpMethodEnum.POST, request,
-                MediaTypes.TEXT_PLAIN);
+                MediaTypes.TEXT_PLAIN_UTF_8);
         int status = httpResponse.getStatus();
         String responseBody = httpResponse.getBody();
-        System.out.println("返回代码:" + status);
-        System.out.println("返回body:");
-        System.out.println(responseBody);
-
-        System.out.println("======================下面的请求获取cookie======================");
-        Integer index = responseBody.indexOf("id=") + 3;
-        Integer last = responseBody.indexOf("&", index);
-        String id = responseBody.substring(index, last);
-        index = responseBody.indexOf("num=");
-        String num = responseBody.substring(index+4);
-//        url =
+//        System.out.println("返回代码:" + status);
+//        System.out.println("返回body:");
+//        System.out.println(responseBody);
 
 
-
-        System.out.println("======================下面是进入教务处======================");
-
-
-        url = "http://59.77.226.35/default.aspx?id=" + id;
-        request = new HttpRequest();
-        request.addHeader("Cookie",cookie);
-        httpResponse = HttpClientUtil.doRequestWithNoRetry(url, HttpMethodEnum.GET, request, MediaTypes.TEXT_PLAIN);
+        System.out.println("======================再次重定向，并且获取新的ID======================");
+        url = httpResponse.getHeaders().get("Location");
+        httpResponse = HttpClientUtil.doRequestWithNoRetry(url, HttpMethodEnum.GET, request, MediaTypes.TEXT_PLAIN_UTF_8);
         status = httpResponse.getStatus();
-        String result = httpResponse.getBody();
-        System.out.println(status);
-        System.out.println(result);
+        responseBody = httpResponse.getBody();
+//        System.out.println(status);
+//        System.out.println(responseBody);
 
-        System.out.println("======================下面是AJAX请求======================");
+        Integer index = responseBody.indexOf("id=") + 3;
+        Integer last = responseBody.indexOf("\"", index);
+        String id = responseBody.substring(index, last);
+
+        System.out.println("======================下面是AJAX获取成绩======================");
         url = "http://59.77.226.35/student/jscp/TeaList.aspx?id=" + id + "&bj=score";
         httpResponse = HttpClientUtil.doRequestWithNoRetry(url, HttpMethodEnum.GET, request, MediaTypes.TEXT_PLAIN);
         status = httpResponse.getStatus();
-        result = httpResponse.getBody();
-        System.out.println(status);
-        System.out.println(result);
+        responseBody = httpResponse.getBody();
+//        System.out.println(status);
+        System.out.println(responseBody);
 
+        String target = "计算机网络体系结构";
+        readScore(responseBody,target);
+    }
 
-        System.out.println("======================下面是AJAX获取成绩======================");
-        url = "http://59.77.226.35/student/xyzk/cjyl/score_sheet.aspx?id=" + id;
-        httpResponse = HttpClientUtil.doRequestWithNoRetry(url, HttpMethodEnum.GET, request, MediaTypes.TEXT_PLAIN);
-        status = httpResponse.getStatus();
-        result = httpResponse.getBody();
-        System.out.println(status);
-        System.out.println(result);
-
-
-        //获取cookie
-//        String cookieHeader = httpResponse.getHeader("Set-Cookie");
-//        Integer index = cookieHeader.indexOf("=");
-//        Integer last = cookieHeader.indexOf(";");
-//        String cookie = "ASP.NET_SessionId=" + cookieHeader.substring(index,last);
-//        System.out.println("COOKIE:" + cookie);
+    public static void readScore(String body,String target) throws MessagingException {
+        String[] t = target.split("\\n");
+        for(int i=0;i<t.length;i++){
+            //找到了目标学科的成绩
+            if(t[i].indexOf(target)!=-1){
+                //判断成绩是不是出来了
+                if(t[i+2].indexOf("成绩尚未录入")!=-1){
+                    //成绩出来了发送邮件通知
+                    SimpleMailSender sms = new SimpleMailSender("249602015@qq.com","joy-zhuang");
+                    String message = "扬哥，您的<font color='red'>" + target + "</font>成绩终于出来啦!您获得了" + t[i+2] + "分!恭喜您通过考试!";
+                    sms.send("249602015@qq.com","成绩出来了!",message);
+                    break;
+                }else{
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/utils/SimpleMail.java
@@ -0,0 +1,25 @@
+package utils;
+
+/**
+ * Created by zhuangjy on 2015/7/13.
+ */
+public class SimpleMail {
+    private String Content;
+    private String Subject;
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String Content) {
+        this.Content = Content;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String Subject) {
+        this.Subject = Subject;
+    }
+}
